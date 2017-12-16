2ae53196683c44b5cf319ee28da41802f22d6dc7
pdrabinski <pdrabinski@gmail.com>
2017-12-16 09:37:02 -0700

    removed link list

--- src/get_urls.py
+++ src/get_urls.py
@@ -10,7 +10,6 @@ def get_product_links(base_url, url_suffix,n_pages, reviews_filename, products_f
 
     """Crawl through product gallery pages and get links for products. Parser only parse a tags. Might be able to remove product link list."""
 
-    product_link_list = []
     only_a_tags = SoupStrainer('a')
 
     j = 0 #track number of product links scraped
@@ -26,22 +25,20 @@ def get_product_links(base_url, url_suffix,n_pages, reviews_filename, products_f
         for a in product_links:
             time.sleep(random.randint(1,2))
             product_url = 'https://www.backcountry.com' + a.get('href')
-            product_link_list.append(product_url)
             get_reviews(product_url, reviews_filename, products_filename)
             print(j, "Products")
             j += 1
-        print("Page {} done, {} links".format(i+1, len(product_link_list)))
+        print("Page {} done, {} links".format(i+1, j))
 
-    print(product_link_list)
-    print(len(product_link_list), "Product Links")
+    print(j, "Product Links")
 
 if __name__ == '__main__':
     #url = 'https://www.backcountry.com/mens-climb-jackets?p=brand%3AArc%27teryx%7Cbrand%3AMarmot%7Cbrand%3APatagonia%7Cbrand%3AOutdoor%5C+Research%7Cbrand%3AThe%5C+North%5C+Face&page='
 
-    url = 'https://www.backcountry.com/womens-climb-jackets?p=brand%3AMarmot%7Cbrand%3APatagonia%7Cbrand%3AArc%27teryx%7Cbrand%3AThe%5C+North%5C+Face%7Cbrand%3AOutdoor%5C+Research&page='
+    url = 'https://www.backcountry.com/marmot?show=all&p=reviewAverage%3A%5B1+TO+*%5D&page='
 
     add_on = '&nf=1'
-    n_pages = 3
-    products_filename = '../data/products.csv'
-    reviews_filename = '../data/reviews.csv'
+    n_pages = 11
+    products_filename = '../data/marmot_products.csv'
+    reviews_filename = '../data/marmot_reviews.csv'
     get_product_links(url, add_on, n_pages, reviews_filename, products_filename)
