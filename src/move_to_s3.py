3a2e386c9847fb6c808ea5ffc735d8cd0a25a5fb
pdrabinski <pdrabinski@gmail.com>
2017-12-19 12:16:32 -0700

    fixed filepath list

--- src/move_to_s3.py
+++ src/move_to_s3.py
@@ -1,5 +1,6 @@
 import boto3
-import os
+from os import listdir
+from os.path import isfile, join
 
 def print_s3_contents_boto3(connection):
     for bucket in connection.buckets.all():
@@ -9,8 +10,12 @@ def print_s3_contents_boto3(connection):
 if __name__ == '__main__':
     boto3_connection = boto3.resource('s3')
     s3_client = boto3.client('s3')
-    bucket_name = 'user_reviews-bucket'
+    bucket_name = 'user-reviews-bucket'
     boto3_connection.create_bucket(Bucket=bucket_name)
-    for f in os.path.join("..","data","reviews"):
-        s3_client.upload_file(f, bucket_name, f)
+    reviews_dir = '../data/reviews/'
+    files = [f for f in listdir(reviews_dir) if isfile(join(reviews_dir,f))]
+    file_paths = [reviews_dir + f for f in listdir(reviews_dir) if isfile(join(reviews_dir,f))]
+    for i,path in enumerate(file_paths):
+        print(path)
+        s3_client.upload_file(path, bucket_name, files[i])
     print_s3_contents_boto3(boto3_connection)
